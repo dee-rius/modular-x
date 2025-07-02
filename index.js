ef324b0c4a01976c8e28373343ec2e1c819e6287
dee-rius <darkwahdarius10@gmail.com>
2025-07-02 20:53:28 +0100

    reinstall: packages. feat: sleep, read and create_or_edit utility functions

--- index.js
+++ index.js
@@ -2,15 +2,48 @@
 
 import * as clack from "@clack/prompts";
 import chalk from "chalk";
-import fs from "fs";
+import fs from "fs/promises";
 
-checkIfEquaionsDirectoryExists()
+const text_encoding = "utf-8";
 
-function getRandomInt(min, max){
+const expression_storage_directory_name = "expressions";
+const expression_storage_file_format = ".txt";
+//func to get user action
+
+//func to read user action
+
+
+//utility functions
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function read(expression_name){
+    try{
+        let expression_storage_file_path = `${expression_storage_directory_name}/${expression_name}${expression_storage_file_format}`;
+        let expression_content = await fs.readFile(expression_storage_file_path, text_encoding);
+        return expression_content;
+    }
+    catch{
+        clack.log.error("Expression not found...");
+        return;
+    }
+}
+
+async function create_or_edit(expression_name, expression_content){
+    try{
+        let expression_storage_file_path = `${expression_storage_directory_name}/${expression_name}${expression_storage_file_format}`;
+        await fs.writeFile(expression_storage_file_path, expression_content, text_encoding);
+    }
+    catch{
+        //fill here;
+    }
+}
+
+
+function get_randomInt(min, max){
     return Math.floor((Math.random() * (max - min)) + min);
 }
 
-function checkIfOtherCMDsInputted(input){
+function check_if_other_cmds_inputted(input){
     if(input.toLowerCase().trim() == "quit"){
         process.exit(0);
     }
