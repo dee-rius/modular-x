049f0397c58c4c7f0747a53b55762fc7cc1c6b88
dee-rius <darkwahdarius10@gmail.com>
2025-07-05 14:22:10 +0100

    feat: get_name_of_equation_to_solve, replacing  of special characters to avoid error (solving expressions)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import * as clack from "@clack/prompts";
 import path from "path";
 import chalk from "chalk";
 import fs from "fs/promises";
+import { networkInterfaces } from "os";
 
 
 const text_encoding = "utf-8";
@@ -18,6 +19,8 @@ const expression_status_response_prefix = "Expression";
 const operation_canceled_text = "Operation cancelled";
 
 //spinner styling
+const busy_spinner_text = "Processing";
+const busy_spinner_complete_text = "Processing complete";
 //stroring
 const expression_storing_spinner_text = "Storing expression";
 const expression_storing_spinner_complete_text = "Rendering expression content...";
@@ -29,6 +32,15 @@ const expression_edit_reading_old_spinner_text = "Getting expression text";
 const expression_edit_reading_old_spinner_complete_text = "Rendering expression content...";
 //renaming
 const expression_rename_complete_spinner_text = "Expression renamed";
+//solving
+const replacing_special_characters_spinner_text = "Encrypting special characters";
+const replacing_special_characters_spinner_complete_text = "Rendering encrypted expression...";
+
+const replacing_variables_spinner_text = "Substituting variables";
+const replacing_variables_spinner_complete_text = "All variables substituted";
+
+const expression_solving_spinner_text = "Evaluating expression";
+const expression_solving_spinner_complete_text = "Expression successfully evaluated";
 
 const expression_not_found_text = "Expression not found";
 
@@ -61,7 +73,7 @@ const available_action_choices = [
     {
         value: "solve",
         hint: "Solve existing expression",
-        corresponding_function: solve_expression,
+        corresponding_function: get_name_of_equation_to_solve,
     },
     {
         value: "rename",
@@ -84,11 +96,11 @@ async function boot() {
     //creates a spinner (for styling purposes)
     await create_spinner("Booting", "Boot complete", 1000);
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
 
     //displays important text
     clack.log.warn(set_text_colour("Note: Ctrl + C to quit/cancel", "warn"))
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     check_if_expression_storage_path_exists()
 }
 
@@ -113,15 +125,15 @@ async function get_user_action_choice() {
         options: available_action_choices,
     })
 
-    if(clack.isCancel(user_action_choice)){
+    if (clack.isCancel(user_action_choice)) {
         clack.cancel(operation_canceled_text);
         process.exit(0);
     }
 
-    for(let available_action_choice of available_action_choices){
-        if(available_action_choice.value == user_action_choice){
+    for (let available_action_choice of available_action_choices) {
+        if (available_action_choice.value == user_action_choice) {
             await create_spinner(`${opening_dialog_text_prefix} [${user_action_choice}] dialogue`, `Rendering expression [${user_action_choice}] dialogue`, 1000);
-            await sleep(500);
+            await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
             await available_action_choice.corresponding_function();
         }
     }
@@ -129,29 +141,29 @@ async function get_user_action_choice() {
 
 
 //action functions
-async function list_expressions(){
-    try{
-        let expression_storage_files = await fs.readdir(expression_storage_directory_path, {withFileTypes: false});
+async function list_expressions() {
+    try {
+        let expression_storage_files = await fs.readdir(expression_storage_directory_path, { withFileTypes: false });
         let expression_storage_file_names = []
-        for(let expression_storage_file of expression_storage_files){
+        for (let expression_storage_file of expression_storage_files) {
             expression_storage_file_names.push(path.basename(expression_storage_file, `.${expression_storage_file_format}`));
         }
         clack.note(expression_storage_file_names.join("\n"), "Stored expressions")
     }
-    catch{
+    catch {
         clack.log.error("Expression storage directory may be missing");
         check_if_expression_storage_path_exists();
     }
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     get_user_action_choice();
 }
 
 async function create_expression() {
     //get_expression_details(get_expression_content = false, action_intent, display_old_content = false)
     let new_expression_details = await get_expression_details(true, "create");
-    
-    if(new_expression_details != undefined){
+
+    if (new_expression_details != undefined) {
         await fs.writeFile(new_expression_details.storage_file_path, new_expression_details.content, text_encoding);
 
         //styling purposes
@@ -159,28 +171,28 @@ async function create_expression() {
         clack.note(new_expression_details.content, new_expression_details.name);
     }
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     get_user_action_choice();
 }
-async function read_expression(){
+async function read_expression() {
     //get_expression_details(get_expression_content = false, action_intent, display_old_content = false)
     let expresion_to_read = await get_expression_details(false, "read");
 
-    if(expresion_to_read != undefined){
+    if (expresion_to_read != undefined) {
         await create_spinner(expression_reading_spinner_text, expression_reading_spinner_complete_text, 500);
         let expression_content = await fs.readFile(expresion_to_read.storage_file_path, text_encoding);
 
         clack.note(expression_content, expresion_to_read.name);
     }
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     get_user_action_choice();
 }
 async function edit_expression() {
     //get_expression_details(get_expression_content = false, action_intent, display_old_content = false)
     let expression_to_edit = await get_expression_details(true, "edit", true);
 
-    if(expression_to_edit != undefined){
+    if (expression_to_edit != undefined) {
         //update file-content
         await fs.writeFile(expression_to_edit.storage_file_path, expression_to_edit.content, text_encoding);
 
@@ -188,14 +200,14 @@ async function edit_expression() {
         clack.note(expression_to_edit.content, expression_to_edit.name);
     }
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     get_user_action_choice();
 }
 async function delete_expression() {
     //get_expression_details(get_expression_content = false, action_intent, display_old_content = false)
     let expression_to_delete = await get_expression_details(false, "delete", true);
 
-    if(expression_to_delete != undefined){
+    if (expression_to_delete != undefined) {
         //check if the user really wants delete it
 
         let delete_choice = await clack.confirm({
@@ -203,56 +215,116 @@ async function delete_expression() {
             initialValue: false,
         })
 
-        if(delete_choice === true){
-            try{
+        if (delete_choice === true) {
+            try {
                 await fs.access(expression_to_delete.storage_file_path);
                 await fs.unlink(expression_to_delete.storage_file_path);
 
-                await create_spinner(`${chalk.red("Permanently")} removing ${expression_to_delete.name}` , `${chalk.red("Permanently")} removed: ${expression_to_delete.name}`, 750);
+                await create_spinner(`${chalk.red("Permanently")} removing ${expression_to_delete.name}`, `${chalk.red("Permanently")} removed: ${expression_to_delete.name}`, 750);
             }
-            catch{
+            catch {
                 clack.log.error(expression_not_found_text);
                 check_if_expression_storage_path_exists();
             }
         }
     }
 
-    await sleep(500);
-    get_user_action_choice();
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
+    //check if teh diresctory exists before moving on to collect user action
+    check_if_expression_storage_path_exists()
 }
 
 async function rename_expression() {
     let expression_to_rename = await get_expression_details(false, "rename", false, "Enter (old) expression name");
 
-    if(expression_to_rename != undefined){
+    if (expression_to_rename != undefined) {
         //get the new name for the expression
         let new_expresion_name = await get_expression_details(false, "get_new_name_for_rename", false, "Enter (new) expression name");
-        
-        if(new_expresion_name != undefined){
+
+        if (new_expresion_name != undefined) {
             //renames the file
-            try{
+            try {
                 await fs.rename(expression_to_rename.storage_file_path, new_expresion_name.storage_file_path);
 
                 await create_spinner(`Changing name from ${expression_to_rename.name} to ${new_expresion_name.name}`, expression_rename_complete_spinner_text, 500);
-                await sleep(500);
+                await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
 
                 //displays editted name and corresponding content afterwards
                 let expression_content = await fs.readFile(new_expresion_name.storage_file_path, text_encoding);
                 clack.note(expression_content, new_expresion_name.name);
             }
-            catch{
-                clack.log.error("Error occured: expression not found")
+            catch {
+                clack.log.error(expression_not_found_text)
             }
         }
-        
+
     }
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     get_user_action_choice();
 }
-async function solve_expression(){
 
+
+
+//solving stages (all functions pass down the expression name) -> split into multiple functions because of the single responsibility rule
+
+//gets name of equation to solve
+async function get_name_of_equation_to_solve(){
+
+    let expression_to_solve = await get_expression_details(false, "solve", true);
+    try{
+       
+        if(expression_to_solve != undefined){
+             //get expression content
+            let expression_content = await fs.readFile(expression_to_solve.storage_file_path, text_encoding);
+            await process_special_characters_and_basic_replacements(expression_content, expression_to_solve);
+        }
+
+    }
+    catch (error){
+        //if not log expression not found
+        clack.log.error(error);
+        check_if_expression_storage_path_exists();
+    }
 }
+
+//func to process equation for bidmas, does actual replacing and passses down the new expression
+async function process_special_characters_and_basic_replacements(expression_content, expression_to_solve){
+    let to_replace = {
+        "^": "**",
+        "_": "**1/",
+    }
+
+    let expression_content_split_by_character = expression_content.split("");
+
+    for(let i = 0; i < expression_content_split_by_character.length - 1; i++){
+        let this_character = expression_content_split_by_character[i];
+        let next_character = expression_content_split_by_character[i + 1];
+
+        //checks if this_character isn't a letter or number. If so, skips to the next iteration
+        if(/^[a-zA-Z]+$/.test(this_character) === false || isNaN(this_character)){
+            continue;
+        }
+        //checks if the nex number is an open bracket or a letter
+        if(next_character == "(" || /^[a-zA-Z]+$/.test(this_character)){
+            //if so, inserts * bettween them (e.g., 2x -> 2*x or x(y - 2) -> x*(y-2))
+            to_replace[`${this_character}${next_character}`] = `${this_character}*${next_character}`;
+        }
+    }
+
+    for(let chars_to_replace in to_replace){
+        expression_content = expression_content.replaceAll(chars_to_replace, to_replace[chars_to_replace]);
+    }
+
+    create_spinner(replacing_special_characters_spinner_text, replacing_special_characters_spinner_complete_text, 500);
+    clack.note(expression_content, expression_to_solve.name);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
+}
+
+//func loops through the list and finds and stores variables in a an array, and passes that down
+//function gets the user to input input replacements for the variables, and stores the variables a keys and the users values as values of the keys in the object and passes that down
+//function does actual replacing of variables and solves the expression, and shows the results with clack.note using the output as the header and the equaition as the footer
+
 
 
 //utility functions
@@ -260,75 +332,75 @@ async function solve_expression(){
 async function get_expression_details(get_expression_content, action_intent, display_old_content, expression_name_propmt_message = "Enter expression name") {
     //gets all sroted files;
     let expression_storage_files = [];
-    try{
+    try {
         expression_storage_files = await fs.readdir(expression_storage_directory_path);
     }
-    catch{
+    catch {
         ///thsi hapens when an error occurs
         check_if_expression_storage_path_exists();
     }
 
     let expression_content_prompt_placeholder = "(x2 - x1)^2 + (y2 - y1)^2";
-    
+
     let expresion_name = await clack.text({
         message: expression_name_propmt_message,
         placeholder: "name",
         //validates depending on the type of action
         validate: (value) => {
-            if (action_intent == "create" ||action_intent == "get_new_name_for_rename") {
-                
+            if (action_intent == "create" || action_intent == "get_new_name_for_rename") {
+
                 //checks if file exists and displays a error mesagge if so
-                if(expression_storage_files.includes(`${value.trim()}.${expression_storage_file_format}`)){
+                if (expression_storage_files.includes(`${value.trim()}.${expression_storage_file_format}`)) {
                     return "Expression already exists";
                 }
             }
-            else{
-                 //checks if file does'nt exist and returns an error
-                 if(!expression_storage_files.includes(`${value.trim()}.${expression_storage_file_format}`)){
+            else {
+                //checks if file does'nt exist and returns an error
+                if (!expression_storage_files.includes(`${value.trim()}.${expression_storage_file_format}`)) {
                     return expression_not_found_text;
                 }
             }
         }
     });
-    if(clack.isCancel(expresion_name)){
+    if (clack.isCancel(expresion_name)) {
         //add spinner
         clack.cancel(operation_canceled_text);
         return undefined;
     }
 
-    await sleep(500);
+    await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     //if action is edit, displays previous file content
     let full_expression_storage_file_path = `${expression_storage_directory_path}/${expresion_name.trim()}.${expression_storage_file_format}`;
-    if(display_old_content === true){
+    if (display_old_content === true) {
 
         create_spinner(expression_edit_reading_old_spinner_text, expression_edit_reading_old_spinner_complete_text, 500);
-        
-        await sleep(500);
-        
+
+        await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
+
         let old_expression_content = await fs.readFile(full_expression_storage_file_path, text_encoding);
         clack.note(old_expression_content, `${expresion_name}`);
-        
+
         //change the placeholder to make sure the user knows the previous value
         expression_content_prompt_placeholder = old_expression_content;
-        
-        await sleep(500);
+
+        await create_spinner(busy_spinner_text, busy_spinner_complete_text,500);
     }
 
     let expression_content = "";
-    if(get_expression_content === true){
-            expression_content = await clack.text({
+    if (get_expression_content === true) {
+        expression_content = await clack.text({
             message: `Enter new content of ${expresion_name}`,
             placeholder: expression_content_prompt_placeholder,
             //if get_expression_content == false, disable = true, disabling it
         })
-        if(clack.isCancel(expression_content)){
+        if (clack.isCancel(expression_content)) {
             clack.cancel(operation_canceled_text);
             return undefined;
         }
     }
 
 
-    return get_expression_content === false? {name: expresion_name.trim(), storage_file_path: full_expression_storage_file_path}: {name: expresion_name.trim(), content: expression_content.trim(), storage_file_path: full_expression_storage_file_path};
+    return get_expression_content === false ? { name: expresion_name.trim(), storage_file_path: full_expression_storage_file_path } : { name: expresion_name.trim(), content: expression_content.trim(), storage_file_path: full_expression_storage_file_path };
 }
 
 //promise-based set timeout
